baa5fae100e3e2b120ffbbca16eef18b8c0885b2
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2023-10-15 17:14:46 +0300

    Changes to the UI

---
+++ peeps/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from . import models
+import base64
+from django.conf import settings
+import os
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Restaurant
+        fields = ['id', 'name', 'direction', 'phone']
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class meta:
+        model = models.Ingredient
+        fields = ['id', 'name']
+        
+class RecipeSerializer(serializers.ModelSerializer):
+    thumnbnail = serializers.SerializerMethodField('encode_thumbnail')
+    ingredients = serializers.SerializerMethodField('get_ingredients')
+    
+    def encode_thumbnail(self, recipe):
+        with open(os.path.join(settings.MEDIA_ROOT, recipe.thumbnail.name), "rb") as image_file:
+            return base64.b64encode(image_file.read())
+        
+    def get_ingredients(self, recipe):
+        try:
+            recipe_ingredients = models.Ingredient.objects.filter(recipe_id=recipe.id)
+            return IngredientSerializer(recipe_ingredients, many=True).data
+        except models.Ingredient.DoesNotExist:
+            return None
+        
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop("ingredients")
+        
+        restaurant = models.Restaurant.objects.get(pk=validated_data["restaurant_id"])
+        validated_data["restaurant"] = restaurant
+        recipe = models.Recipe.objects.create(**validated_data)
+        
+        if ingredients_data:
+            for ingredient_dict in ingredients_data:
+                ingredient = models.Ingredient(name=ingredient_dict["name"])
+                ingredient.save()
+                ingredient.recipe.add(recipe)
+        return recipe
+    
+    class meta:
+        model = models.Recipe
+        fields = ['id', 'name', 'type', 'thumbnail', 'ingredients']
